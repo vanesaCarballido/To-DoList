bdca19776ebeb599a7dfd193d89c0b0b5bfe3ce4
unknown <vanesa.carballido@correo.ucu.edu.uy>
2025-10-01 09:17:06 -0300

    faltan funciones de TaskList

--- vite-project/src/components/TaskItem.jsx
+++ vite-project/src/components/TaskItem.jsx
@@ -1,13 +1,15 @@
 import React, { useMemo } from "react";
 
 function TaskItem ({ task, onComplete, onDelete}){ 
-    return( <div className="taskTextButton">
-        {task.text}
-        <span>
-            <button onClick={()=> onComplete(task.id)}>Completada</button>
-            <button onClick={()=> onDelete(task.id)}>Borrar</button>
-         </span>
-    </div>
-)};
+    return ( 
+        <div className="taskTextButton">
+            {task.text}
+            <span>
+                <button onClick={()=> onComplete(task.id)}>Completada</button>
+                <button onClick={()=> onDelete(task.id)}>Borrar</button>
+            </span>
+        </div>
+    )
+};
 
 export default React.memo(TaskItem);
