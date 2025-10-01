0ea5c5c3c7ca713d28104143d007b37459e460a3
belenkanas <maria.kanas@correo.ucu.edu.uy>
2025-10-01 08:38:09 -0300

    utils cambio

--- ToDo-List/src/components/utils.jsx
+++ ToDo-List/src/components/utils.jsx
@@ -11,11 +11,6 @@ export function createTodos() {
 }
 
 export function filterTodos(todos, tab) {
-  console.log('[ARTIFICIALLY SLOW] Filtering ' + todos.length + ' todos for "' + tab + '" tab.');
-  let startTime = performance.now();
-  while (performance.now() - startTime < 500) {
-    // Do nothing for 500 ms to emulate extremely slow code
-  }
 
   return todos.filter(todo => {
     if (tab === 'all') {
